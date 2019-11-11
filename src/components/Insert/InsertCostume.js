6fbe4840cc7e8719af9c5b99e9c26a431de9d7f1
Eirini Kaklopoulou <eirini.kaklopoulou@gmail.com>
2019-11-11 17:02:11 +0200

    Changes for paths

--- src/components/Insert/InsertCostume.js
+++ src/components/Insert/InsertCostume.js
@@ -528,7 +528,7 @@ class InsertCostume extends Component {
                             closeMenuOnSelect={true}  
                             isSearchable            
                         /> 
-                        <a href='/insert-theatrical_play'> Προσθήκη νέας παράστασης</a>
+                        <a href='/insert-tp'> Προσθήκη νέας παράστασης</a>
                         </Form.Field>
                     <Form.Group widths="equal">
                         <Form.Field>
